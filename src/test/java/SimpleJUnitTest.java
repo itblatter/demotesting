ff48185a71e6c852bd86829cd2db9ab173a46f01
Daniil Ruban <ITBlatter@MacBook-Pro-Maria.local>
2026-01-13 09:12:16 +0300

    add SimpleJUnitTest

---
+++ src/test/java/SimpleJUnitTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SimpleJUnitTest {
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("### BeforeEach: 3");
+        Assertions.assertEquals(3, 3); // проверка
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("### AfterEach: 3");
+        Assertions.assertEquals(3, 3); // проверка
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("### firstTest()");
+        Assertions.assertTrue(3 > 2);
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("### secondTest()");
+        Assertions.assertTrue(3 > 2);
+    }
+
+    @Test
+    void thirdTest() {
+        System.out.println("### thirdTest()");
+        Assertions.assertTrue(3 > 2);
+    }
+}
